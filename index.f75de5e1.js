d1c7fabc0bcb41b65446003813306999117698ae
Mariana Trinko <marjanavv@gmail.com>
2024-07-19 17:49:10 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-shadow */ "use strict";
+const mainImage = document.querySelector("#largeImg");
+const thumbsUl = document.querySelector("#thumbs");
+const imageHandler = (event)=>{
+    event.preventDefault();
+    const link = event.target.closest(".list-item__link");
+    mainImage.src = link.href;
+};
+thumbsUl.addEventListener("click", imageHandler);
+
+//# sourceMappingURL=index.f75de5e1.js.map
